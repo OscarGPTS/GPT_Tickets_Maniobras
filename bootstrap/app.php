38f78c200cd0b18c3adefdada2f369a977394929
OscarGPTS <ochavez@gptservices.com>
2026-02-05 11:57:15 -0600

    fix http conexion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Confiar en proxies de Cloudflare
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
